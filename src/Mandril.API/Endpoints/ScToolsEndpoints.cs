34eb33b75f0ef9ae94259f5178cb2e0c8f1508cd
Lother88 <manrik88@gmail.com>
2023-12-29 00:27:14 +0100

    GSWB-65 Add new endpoint for get RSI web Data

---
+++ src/Mandril.API/Endpoints/ScToolsEndpoints.cs
@@ -0,0 +1,39 @@
+﻿using Mandril.Application;
+using Mandril.Domain.ValueObjects;
+using TGF.CA.Presentation;
+using TGF.CA.Presentation.Middleware;
+using TGF.CA.Presentation.MinimalAPI;
+
+namespace Maindril.API.Endpoints
+{
+    /// <inheritdoc/>
+    public class ScToolsEndpoints : IEndpointDefinition
+    {
+
+        #region IEndpointDefinition
+
+        /// <inheritdoc/>
+        public void DefineEndpoints(WebApplication aWebApplication)
+        {
+            aWebApplication.MapGet(MandrilApiRoutes.sc_tools_list_ships, GetListShips).SetResponseMetadata<ScShip[]>(200);
+        }
+
+        /// <inheritdoc/>
+        public void DefineRequiredServices(IServiceCollection aRequiredServicesCollection)
+        {
+        }
+
+        #endregion
+
+        #region EndpointMethods
+
+        /// <summary>
+        /// Get all ship data from RSI web and available CCU in json format.
+        /// </summary>
+        private async Task<IResult> GetListShips(IScToolsService aScToolsService, CancellationToken aCancellationToken = default)
+            => await aScToolsService.GetRsiShipList().ToIResult();
+
+        #endregion
+
+    }
+}
