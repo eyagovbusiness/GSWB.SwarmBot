599a6e13c992877160a81d4899b91558c2972e51
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-03-09 15:17:17 +0100

    GSWB-92 Change profile image to 256

--- src/SwarmBot/Extensions/DiscordMemberExtensions.cs
+++ src/SwarmBot/Extensions/DiscordMemberExtensions.cs
@@ -5,8 +5,8 @@ namespace SwarmBot.Extensions
 {
     public static class DiscordMemberExtensions
     {
-        public const int DefaultAvatarSize = 64;
-        public const ImageFormat DefaultAvatarFormat = ImageFormat.Jpeg;
+        public const ushort DefaultAvatarSize = 256;
+        public const ImageFormat DefaultAvatarFormat = ImageFormat.Png;
         public static string GetGuildAvatarUrlOrDefault(this DiscordMember aDiscordMember)
         => aDiscordMember.GetGuildAvatarUrl(DefaultAvatarFormat, DefaultAvatarSize)
         ?? aDiscordMember.DefaultAvatarUrl;
