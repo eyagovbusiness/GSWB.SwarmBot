61487ca70e4baa0c13cce76ae69f04c8c44a68cf
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-10-12 16:13:58 +0200

    Removed legacy GuildsHandler fields and methods

--- src/SwarmBot/Handlers/GuildsHandler.cs
+++ src/SwarmBot/Handlers/GuildsHandler.cs
@@ -11,28 +11,16 @@ namespace SwarmBot.Handlers
     internal class GuildsHandler
     {
         private readonly DiscordClient _client;
-        private readonly ulong _guildId;
-        private readonly ulong _testersGuildId;
+
         public GuildsHandler(ISwarmBotDiscordBot aSwarmBotDiscordBot)
         {
             var lSwarmBotDiscordBot = aSwarmBotDiscordBot as SwarmBotDiscordBot;
             _client = lSwarmBotDiscordBot?.Client ?? throw new ArgumentNullException(nameof(aSwarmBotDiscordBot));
-            _guildId = lSwarmBotDiscordBot.BotConfiguration.DiscordTargetGuildId;
-            _testersGuildId = lSwarmBotDiscordBot.BotConfiguration.TestersDiscordTargetGuildId;
         }
 
         public async Task<IHttpResult<DiscordGuild>> GetGuildById(ulong id, bool callAPI, CancellationToken cancellationToken = default)
              => await Result.CancellationTokenResultAsync(cancellationToken)
             .Map(_ => _client.GetGuildAsync(id, callAPI))
-            .Verify(discordGuild => discordGuild != null, DiscordBotErrors.Guild.NotFoundId);
-
-        public async Task<IHttpResult<DiscordGuild>> GetDiscordGuildFromConfigAsync(CancellationToken aCancellationToken = default, bool? aCallAPI = null)
-            => await Result.CancellationTokenResultAsync(aCancellationToken)
-            .Map(_ => _client.GetGuildAsync(_guildId, aCallAPI))
-            .Verify(discordGuild => discordGuild != null, DiscordBotErrors.Guild.NotFoundId);
-        public async Task<IHttpResult<DiscordGuild>> GetTestersDiscordGuildFromConfigAsync(CancellationToken aCancellationToken = default, bool? aCallAPI = null)
-            => await Result.CancellationTokenResultAsync(aCancellationToken)
-            .Map(_ => _client.GetGuildAsync(_testersGuildId, aCallAPI))
             .Verify(discordGuild => discordGuild != null, DiscordBotErrors.Guild.NotFoundId);
 
         public async Task<IHttpResult<IEnumerable<DiscordGuild>>> GetUserGuildListAsync(ulong userId, CancellationToken cancellationToken = default)
