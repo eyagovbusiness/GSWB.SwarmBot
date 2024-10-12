46ccf10775d9915334c56ac295a6a8b05755c749
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-10-12 16:07:55 +0200

    Introduced login fixes and adding guild consumer fix

--- src/SwarmBot.API/Endpoints/PrivateEndpoints.cs
+++ src/SwarmBot.API/Endpoints/PrivateEndpoints.cs
@@ -5,8 +5,6 @@ using SwarmBot.Application;
 using TGF.CA.Presentation.MinimalAPI;
 using Common.Application.DTOs.Guilds;
 using TGF.CA.Presentation.Middleware;
-using TGF.CA.Infrastructure.Security.Identity.Authentication;
-using System.Security.Claims;
 using Common.Application.DTOs.Discord;
 using TGF.Common.ROP.Result;
 using TGF.Common.ROP.HttpResult;
@@ -19,7 +17,7 @@ namespace SwarmBot.API.Endpoints
         /// <inheritdoc/>
         public void DefineEndpoints(WebApplication aWebApplication)
         {
-            aWebApplication.MapGet(SwarmBotApiRoutes.private_users_me_guilds, Get_UserGuilds).RequireDiscord().SetResponseMetadata<GuildDTO[]>(200).ProducesValidationProblem();
+            aWebApplication.MapGet(SwarmBotApiRoutes.private_users_me_guilds, Get_UserGuilds).SetResponseMetadata<GuildDTO[]>(200).ProducesValidationProblem();
             aWebApplication.MapGet(SwarmBotApiRoutes.private_guilds_roles, Get_GuildServerRoles).SetResponseMetadata<DiscordRoleDTO[]>(200);
 
         }
@@ -33,16 +31,17 @@ namespace SwarmBot.API.Endpoints
         /// <summary>
         /// Gets the list of guilds where both the SwarmBot and the user under the provided id are in.
         /// </summary>
-        private async Task<IResult> Get_UserGuilds(ClaimsPrincipal claimsPrincipal, DiscordIdValidator discordIdValidator, ISwarmBotUsersService swarmBotUsersService, CancellationToken aCancellationToken = default)
-        => await swarmBotUsersService.GetUserGuilds(ulong.Parse(claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier)!), aCancellationToken)
+        private async Task<IResult> Get_UserGuilds(string id, DiscordIdValidator discordIdValidator, ISwarmBotUsersService swarmBotUsersService, CancellationToken aCancellationToken = default)
+        => await Result.ValidationResult(discordIdValidator.Validate(id))
+        .Bind(_ => swarmBotUsersService.GetUserGuilds(ulong.Parse(id), aCancellationToken))
         .ToIResult();
 
         /// <summary>
         /// Gets a list with all the available roles in the guild's server.
         /// </summary>
-        private async Task<IResult> Get_GuildServerRoles(string id, DiscordIdValidator discordIdValidator, ISwarmBotRolesService aSwarmBotRolesService, CancellationToken aCancellationToken = default)
-        => await Result.ValidationResult(discordIdValidator.Validate(id))
-        .Bind(_ => aSwarmBotRolesService.GetGuildServerRoleList(ulong.Parse(id), aCancellationToken))
+        private async Task<IResult> Get_GuildServerRoles(string guildId, DiscordIdValidator discordIdValidator, ISwarmBotRolesService aSwarmBotRolesService, CancellationToken aCancellationToken = default)
+        => await Result.ValidationResult(discordIdValidator.Validate(guildId))
+        .Bind(_ => aSwarmBotRolesService.GetGuildServerRoleList(ulong.Parse(guildId), aCancellationToken))
         .ToIResult();
 
     }
